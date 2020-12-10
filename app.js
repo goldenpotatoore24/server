30b2f2b19c92086f74769c6478fe28c197dd9db8
goldenpotatoore24 <71145660+goldenpotatoore24@users.noreply.github.com>
2020-12-09 18:57:21 -0800

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var serv = require('http').Server(app);
 
 app.get('/', function (req, res){
 
-res.sendFile(__dirname + "/client/index.html");
+res.sendFile(__dirname + "index.html");
 });
 app.use('/client', express.static(__dirname + '/client'));
 serv.listen(2000);
